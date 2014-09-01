f8197406e775f8ff0edd1e2517f3756893a9f378
Ryan Riley <ryan@tachyus.com>
2014-09-01 10:46:28 -0500

    Add OWIN, OWIN hosting, and CORS

---
+++ TodoBackendFSharp/Startup.fs
@@ -0,0 +1,13 @@
+﻿namespace TodoBackendFSharp
+
+open Owin
+open Microsoft.Owin
+
+type Startup() =
+
+    member __.Configuration(app: IAppBuilder) =
+        app .UseCors(Cors.CorsOptions.AllowAll)
+            |> ignore
+
+[<assembly: OwinStartupAttribute(typeof<Startup>)>]
+do ()
