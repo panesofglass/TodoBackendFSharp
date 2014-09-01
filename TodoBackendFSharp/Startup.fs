3f2fb6ee014ee503e041130f93716b2aa387c4b2
Ryan Riley <ryan@tachyus.com>
2014-09-01 11:24:50 -0500

    Add GET handler to return Todo items as JSON.

--- TodoBackendFSharp/Startup.fs
+++ TodoBackendFSharp/Startup.fs
@@ -1,12 +1,40 @@
-﻿namespace TodoBackendFSharp
+﻿//----------------------------------------------------------------------------
+//
+// Copyright (c) 2014 Ryan Riley (@panesofglass)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------------
 
+namespace TodoBackendFSharp
+
+open System
 open Owin
 open Microsoft.Owin
 
 type Startup() =
 
-    member __.Configuration(app: IAppBuilder) =
-        app .UseCors(Cors.CorsOptions.AllowAll)
+    // Store some todos for the initial request
+    let initialTodo : TodoBackend.Todo =
+        { Uri = Uri("/1", UriKind.Relative)
+          Title = "Create todo items"
+          Completed = false
+          Order = 1 }
+    do TodoBackend.todoStorage.Post (TodoBackend.TodoOperation.Post initialTodo)
+
+    member __.Configuration(builder: IAppBuilder) =
+        builder
+            .UseCors(Cors.CorsOptions.AllowAll)
+            .Use(fun _ -> Dyfrig.OwinAppFunc.fromOwinApp TodoBackend.getTodos)
             |> ignore
 
 [<assembly: OwinStartupAttribute(typeof<Startup>)>]
--- TodoBackendFSharp/TodoBackend.fs
+++ TodoBackendFSharp/TodoBackend.fs
@@ -1,4 +1,21 @@
-﻿module TodoBackendFSharp.TodoBackend
+﻿//----------------------------------------------------------------------------
+//
+// Copyright (c) 2014 Ryan Riley (@panesofglass)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------------
+
+module TodoBackendFSharp.TodoBackend
 
 open System
 
@@ -37,3 +54,18 @@ let todoStorage =
                     return! loop todos'
             }
         loop [])
+
+open System.IO
+open Dyfrig
+open Newtonsoft.Json
+
+let serializerSettings = JsonSerializerSettings(ContractResolver = Serialization.CamelCasePropertyNamesContractResolver())
+let serialize data =
+    JsonConvert.SerializeObject(data, serializerSettings)
+    |> Text.Encoding.UTF8.GetBytes
+
+let getTodos (env: OwinEnv) = async {
+    let! todos = todoStorage.PostAndAsyncReply(fun ch -> GetAll ch)
+    let result = serialize todos 
+    let stream : Stream = unbox env.[Constants.responseBody]
+    do! stream.AsyncWrite result }
